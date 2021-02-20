2a71ccc630d22b13812d305e004c7416f2785037
Andrew-M-C <laplacezhang@126.com>
2021-02-20 15:32:37 +0800

    [*] test: rename At() example function names to meet pkg.go.dev

--- example_jsonvalue_test.go
+++ example_jsonvalue_test.go
@@ -106,7 +106,7 @@ func ExampleInsert_Before() {
 // For a simplest example:
 //
 // 这是最简单的例子：
-func ExampleSet_At_1() {
+func ExampleSet_At() {
 	v := jsonvalue.NewObject()                        // {}
 	v.SetObject().At("obj")                           // {"obj":{}}
 	v.SetString("Hello, world!").At("obj", "message") // {"obj":{"message":"Hello, world!"}}
@@ -118,7 +118,7 @@ func ExampleSet_At_1() {
 // Or you can make it even more simpler, as At() function will automatically create objects those do not exist
 //
 // 或者你还可以更加简洁，因为 At() 函数会自动创建在值链中所需要但未创建的对象
-func ExampleSet_At_2() {
+func ExampleSet_At_another() {
 	v := jsonvalue.NewObject()                        // {}
 	v.SetString("Hello, world!").At("obj", "message") // {"obj":{"message":"Hello, world!"}}
 	fmt.Println(v.MustMarshalString())
@@ -129,7 +129,7 @@ func ExampleSet_At_2() {
 // As for array, At() also works
 //
 // 对于数组类型，At() 也是能够自动生成的
-func ExampleSet_At_3() {
+func ExampleSet_At_another2() {
 	v := jsonvalue.NewObject()                // {}
 	v.SetString("Hello, world!").At("arr", 0) // {"arr":[Hello, world!]}
 	fmt.Println(v.MustMarshalString())
@@ -145,7 +145,7 @@ func ExampleSet_At_3() {
 // 在 At() 自动创建数组的逻辑其实稍微有点复杂，需要解释一下。当调用方在参数中指定在某个尚未存在的数组中设置一个值的时候，那么 At() 指定的位置（position）数字，
 // 应当为0，操作才能成功；而当数组已经存在，那么 At() 指定的位置数，要么在数组中已存在，要么正好等于数组的长度，当后者的情况下，会在数组的最后追加值。
 // 这个特性在使用 for-range 块时会非常有用。
-func ExampleSet_At_4() {
+func ExampleSet_At_another3() {
 	v := jsonvalue.NewObject()                         // {}
 	_, err := v.SetString("Hello, world").At("arr", 1) // failed because there are no children of v.arr
 	if err != nil {
@@ -170,7 +170,7 @@ func ExampleSet_At_4() {
 // As for elements those in positions that the array already has, At() will REPLACE it.
 //
 // 正如上文所述，如果在 At() 中指定了已存在的数组的某个位置，那么那个位置上的值会被替换掉，请注意。
-func ExampleSet_At_5() {
+func ExampleSet_At_another4() {
 	v := jsonvalue.NewObject()
 	for i := 0; i < 10; i++ {
 		v.SetInt(i).At("arr", i)
